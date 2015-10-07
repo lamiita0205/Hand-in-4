404a8d2d74857e13d9d65f761c0db52d4ceff933
Dan <idandrei94@gmail.com>
2015-10-07 19:19:54 +0200

    most certainly did some changes around, to no avail

--- app/src/main/java/com/example/lam/handin4/UpdateWidgetService.java
+++ app/src/main/java/com/example/lam/handin4/UpdateWidgetService.java
@@ -21,31 +21,31 @@ public class UpdateWidgetService extends Service {
     {
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
                     .getApplicationContext());
-
             int[] allWidgetIds = intent
                     .getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
 
+            RemoteViews remoteViews = new RemoteViews(this
+                .getApplicationContext().getPackageName(),
+                R.layout.nearby_layout);
+            // Register an onClickListener
+            Intent clickIntent = new Intent(this.getApplicationContext(),
+                nearby_class.class);
+
+            clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
+                allWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.btn, pendingIntent);
+
             for (int widgetId : allWidgetIds) {
 
-                RemoteViews remoteViews = new RemoteViews(this
-                        .getApplicationContext().getPackageName(),
-                        R.layout.nearby_layout);
                 // Set the text
                 remoteViews.setTextViewText(R.id.update,
                         "Current: " + String.valueOf(getPicturesNearby()));
-
-                // Register an onClickListener
-                Intent clickIntent = new Intent(this.getApplicationContext(),
-                        nearby_class.class);
-
-                clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-                clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
-                        allWidgetIds);
-
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, clickIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT);
-                remoteViews.setOnClickPendingIntent(R.id.btn, pendingIntent);
                 appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
         }
         return START_NOT_STICKY;
     }
--- app/src/main/java/com/example/lam/handin4/nearby_class.java
+++ app/src/main/java/com/example/lam/handin4/nearby_class.java
@@ -18,15 +18,38 @@ import java.util.Date;
  */
 public class nearby_class extends AppWidgetProvider {
 
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidhetIds)
+    public static final String WIDGET_IDS_KEY ="mywidgetproviderwidgetids";
+    public static final String WIDGET_DATA_KEY ="mywidgetproviderwidgetdata";
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.hasExtra(WIDGET_IDS_KEY)) {
+            int[] ids = intent.getExtras().getIntArray(WIDGET_IDS_KEY);
+            if (intent.hasExtra(WIDGET_DATA_KEY)) {
+                Object data = intent.getExtras().getParcelable(WIDGET_DATA_KEY);
+                this.update(context, AppWidgetManager.getInstance(context), ids, data);
+            } else {
+                this.onUpdate(context, AppWidgetManager.getInstance(context), ids);
+            }
+        } else super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        update(context, appWidgetManager, appWidgetIds, null);
+    }
+
+    public void update(Context context, AppWidgetManager manager, int[] ids, Object data)
     {
-        //I suppose put the image there, or whateber i can do?!
-        for(int i=0;i<appWidhetIds.length;i++) {
-            int currentWidgetId = appWidhetIds[i];
+        //I suppose put the image there, or whatever i can do?!
+        for(int i=0;i<ids.length;i++) {
+            int currentWidgetId = ids[i];
 
             Intent intent = new Intent(context.getApplicationContext(),
                     UpdateWidgetService.class);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidhetIds);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
 
             // Update the widgets via the service
             context.startService(intent);
