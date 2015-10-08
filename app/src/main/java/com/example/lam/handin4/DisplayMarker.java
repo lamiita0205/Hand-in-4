867495adc4e879d91c1b1551e112cd344e2b8652
lamiita <lamiita0205@gmail.com>
2015-10-08 18:29:28 +0200

    still need widget current location and access all pictures from gmap

--- app/src/main/java/com/example/lam/handin4/DisplayMarker.java
+++ app/src/main/java/com/example/lam/handin4/DisplayMarker.java
@@ -74,6 +74,7 @@ public class DisplayMarker extends AppCompatActivity {
         notes.setText(text);
     }
 
+    
     private void savePreferences(String key, boolean value) {
         SharedPreferences sharedPreferences = PreferenceManager
                 .getDefaultSharedPreferences(this);
